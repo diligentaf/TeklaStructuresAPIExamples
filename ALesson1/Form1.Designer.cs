7597a84b5f2aeee94513c450c58b538425aefba6
moog.kwon <diligentaf@gmail.com>
2023-12-05 13:41:36 +0900

    added parameter

--- ALesson1/Form1.Designer.cs
+++ ALesson1/Form1.Designer.cs
@@ -30,33 +30,59 @@ namespace ALesson1
         private void InitializeComponent()
         {
             this.drw_beam = new System.Windows.Forms.Button();
+            this.create_polybeam_click = new System.Windows.Forms.Button();
+            this.profile_text = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // drw_beam
             // 
-            this.drw_beam.Location = new System.Drawing.Point(273, 137);
+            this.drw_beam.Location = new System.Drawing.Point(318, 45);
+            this.drw_beam.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.drw_beam.Name = "drw_beam";
-            this.drw_beam.Size = new System.Drawing.Size(226, 135);
+            this.drw_beam.Size = new System.Drawing.Size(176, 40);
             this.drw_beam.TabIndex = 1;
-            this.drw_beam.Text = "Draw Button";
+            this.drw_beam.Text = "빔 그리기";
             this.drw_beam.UseVisualStyleBackColor = true;
             this.drw_beam.Click += new System.EventHandler(this.button2_Click);
             // 
+            // create_polybeam_click
+            // 
+            this.create_polybeam_click.Location = new System.Drawing.Point(318, 105);
+            this.create_polybeam_click.Name = "create_polybeam_click";
+            this.create_polybeam_click.Size = new System.Drawing.Size(180, 36);
+            this.create_polybeam_click.TabIndex = 2;
+            this.create_polybeam_click.Text = "폴리빔 그리기";
+            this.create_polybeam_click.UseVisualStyleBackColor = true;
+            this.create_polybeam_click.Click += new System.EventHandler(this.create_polybeam_click_Click);
+            // 
+            // profile_text
+            // 
+            this.profile_text.Location = new System.Drawing.Point(114, 105);
+            this.profile_text.Name = "profile_text";
+            this.profile_text.Size = new System.Drawing.Size(160, 35);
+            this.profile_text.TabIndex = 3;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1486, 900);
+            this.Controls.Add(this.profile_text);
+            this.Controls.Add(this.create_polybeam_click);
             this.Controls.Add(this.drw_beam);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button drw_beam;
+        private System.Windows.Forms.Button create_polybeam_click;
+        private System.Windows.Forms.TextBox profile_text;
     }
 }
 
--- ALesson1/Form1.cs
+++ ALesson1/Form1.cs
@@ -8,11 +8,14 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using tsm = Tekla.Structures.Model;
+using _point = Tekla.Structures.Geometry3d.Point;
+using System.Collections;
 
 namespace ALesson1
 {
     public partial class Form1 : Form
     {
+        Tekla.Structures.Model.Model model = new Tekla.Structures.Model.Model();
         public Form1()
         {
             InitializeComponent();
@@ -20,14 +23,48 @@ namespace ALesson1
 
         private void button2_Click(object sender, EventArgs e)
         {
-            System.Console.WriteLine("yoyoyo this is moog");
-            Tekla.Structures.Model.Model model = new Tekla.Structures.Model.Model();
-            tsm.Beam b1 = new tsm.Beam();
+            Tekla.Structures.Model.UI.Picker pick = new tsm.UI.Picker();
+            _point start = pick.PickPoint("Pick the starting point");
+            _point end = pick.PickPoint("Pick the starting point");
+            create_beam(start, end);
+            model.CommitChanges();
+            MessageBox.Show("Done");
+        }
+        private void create_beam(_point start, _point end)
+        {
+            tsm.Beam b1 = new tsm.Beam(start, end);
             b1.Profile.ProfileString = "WI300-15-20*300";
-            b1.StartPoint = new Tekla.Structures.Geometry3d.Point(0, 0, 0);
-            b1.EndPoint = new Tekla.Structures.Geometry3d.Point(6000, 0, 0);
+            //b1.StartPoint = start;
+            //b1.EndPoint = end;
+
             b1.Insert();
             model.CommitChanges();
+        }
+
+        private void create_polybeam(string profile)
+        {
+            tsm.UI.Picker _pick = new tsm.UI.Picker();
+            ArrayList points = _pick.PickPoints(tsm.UI.Picker.PickPointEnum.PICK_POLYGON, "Please Pick PolyPoint");
+            ArrayList contourpoints = new ArrayList();
+            foreach (_point xx in points)
+            {
+                tsm.ContourPoint cp = new tsm.ContourPoint(xx, new tsm.Chamfer());
+                contourpoints.Add(cp);
+            }
+
+            tsm.ContourPlate cpplate = new tsm.ContourPlate();
+            cpplate.Contour.ContourPoints = contourpoints;
+            cpplate.Profile.ProfileString = profile;
+            cpplate.Material.MaterialString = "S275JR";
+            cpplate.Class = "99";
+            cpplate.Insert();
+            model.CommitChanges();
+            MessageBox.Show("Done");
+        }
+
+        private void create_polybeam_click_Click(object sender, EventArgs e)
+        {
+            create_polybeam(profile_text.Text);
         }
     }
 }
